1fc6a1e2f6127c6b5af5c9c03f2325a37a3d3476
Oscar van Zijverden <o.vanzijverden@gmail.com>
2021-01-05 09:44:39 +0100

    Set up TypeScript & convert files

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Next.js starter</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1>Personal Next.js starter!</h1>
+      {/* TODO: Add list of all libraries here too */}
+    </main>
+  </>
+);
+
+export default Home;
