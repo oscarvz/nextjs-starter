2111dcd4cb72db7add7e7081d56bb9237df1a1a3
Oscar van Zijverden <o.vanzijverden@gmail.com>
2021-01-05 11:57:03 +0100

    Add basic head, CSS reset, global style and theme provider to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,24 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import cssReset from 'styled-reset';
 
-const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+import { theme } from '@styles';
 
-export default MyApp;
+const GlobalStyle = createGlobalStyle(() => cssReset);
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <meta name="description" content="Next.js starter" />
+    </Head>
+
+    <GlobalStyle />
+
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+);
+
+export default App;
