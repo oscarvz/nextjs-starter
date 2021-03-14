4495ed02bc4bc1c942015fa4b50581f8359a6573
Oscar van Zijverden <o.vanzijverden@gmail.com>
2021-03-14 16:27:54 +0100

    Add simple example API call

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,10 +1,10 @@
 import styled, { css } from 'styled-components';
 
-const Button = styled.button(
-  ({ theme }) => css`
+const Button = styled.button<{ primary?: boolean }>(
+  ({ theme, primary }) => css`
     padding: 0.5rem;
     color: ${theme.colors.primaryDark};
-    background-color: ${theme.colors.primary};
+    background-color: ${primary ? theme.colors.primary : theme.colors.white};
     box-shadow: ${theme.shadow.small};
     border: none;
     border-radius: 0.25rem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
 import { Button, Layout } from '@components';
 
-const Home: React.FC = () => (
-  <Layout title="Next.js starter home">
-    <h1>Personal Next.js starter!</h1>
-    {/* TODO: Add list of all libraries here too */}
+const Home: React.FC = () => {
+  const [name, setName] = useState(null);
 
-    <Button>click me</Button>
-  </Layout>
-);
+  const callApi = async (): Promise<void> => {
+    try {
+      const res = await fetch('/api/login');
+
+      if (res.ok) {
+        const data = await res.json();
+        setName(data.name);
+      }
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
+  return (
+    <Layout title="Next.js starter home">
+      <h1>Personal Next.js starter!</h1>
+
+      {/* TODO: Add list of all libraries here too */}
+
+      {!name ? (
+        <Button primary onClick={callApi}>
+          click me
+        </Button>
+      ) : (
+        <h1>Welcome {name}!</h1>
+      )}
+    </Layout>
+  );
+};
 
 export default Home;
