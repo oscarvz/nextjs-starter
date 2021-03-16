ac1672979b7c15144b39c6fea927a5e64bd7fc09
Oscar van Zijverden <o.vanzijverden@gmail.com>
2021-03-16 08:54:05 +0100

    Add more libraries to API & add next.config pointing to external img sources

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['buttercms.com', 'www.mlguides.com', 'styled-components.com', 'prettier.io'],
+  },
+};
--- pages/api/libraries.ts
+++ pages/api/libraries.ts
@@ -3,12 +3,35 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
   res.statusCode = 200;
+
   res.json([
     {
+      name: 'Next.js',
+      description:
+        'Next.js gives you the best developer experience with all the features you need for production: hybrid static & server rendering, TypeScript support, smart bundling, route pre-fetching, and more. No config needed.',
+      url: 'https://nextjs.org',
+      logo: 'https://buttercms.com/static/images/tech_banners/Nextjs.b8a717322c08.png',
+    },
+    {
       name: 'TypeScript',
-      description: 'lorem',
+      description:
+        'TypeScript extends JavaScript by adding types. By understanding JavaScript, TypeScript saves you time catching errors and providing fixes before you run code. Any browser, any OS, anywhere JavaScript runs. Entirely Open Source.',
       url: 'https://typescriptlang.org',
       logo: 'https://www.mlguides.com/wp-content/uploads/2019/08/typescript-logo-500x500.png',
+    },
+    {
+      name: 'Styled Components',
+      description:
+        'styled-components is the result of wondering how we could enhance CSS for styling React component systems. By focusing on a single use case we managed to optimize the experience for developers as well as the output for end user',
+      url: 'https://styled-components.com',
+      logo: 'https://styled-components.com/logo.png',
+    },
+    {
+      name: 'Prettier',
+      description:
+        'Prettier is an opinionated code formatter which removes all original styling and ensures that all outputted code conforms to a consistent style.',
+      url: 'https://prettier.io',
+      logo: 'https://prettier.io/icon.png',
     },
   ]);
 };
