454d635596b95c35c8b8c86ac6b27d1f4ed432d3
Oscar van Zijverden <o.vanzijverden@gmail.com>
2021-05-22 11:49:50 +0200

    Fix remaining 'any' types in _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,22 @@
-import Document, { Html, Head, Main, NextScript, DocumentInitialProps } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentInitialProps,
+  DocumentContext,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: any): Promise<DocumentInitialProps> {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          // TODO: fix the 'any' typings here - couldn't find any resources online how to type this
-          enhanceApp: (App: React.FC) => (props: any) => sheet.collectStyles(<App {...props} />),
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
 
       const initialProps = await Document.getInitialProps(ctx);
