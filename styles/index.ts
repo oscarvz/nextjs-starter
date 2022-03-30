ec9f0f80ed184bad53931a29f643d027414f7b7e
oscarvz <33732524+oscarvz@users.noreply.github.com>
2022-03-30 22:43:11 +0200

    Refactor media query theme function

--- styles/index.ts
+++ styles/index.ts
@@ -1,52 +1,48 @@
-import { css } from 'styled-components';
-import { theme } from './theme';
-
 import type {
   CSSObject,
-  DefaultTheme,
-  FlattenInterpolation,
-  Interpolation,
-  InterpolationFunction,
-  ThemedStyledProps,
+  FlattenSimpleInterpolation,
+  SimpleInterpolation,
 } from 'styled-components';
 
-type CSSPropArguments =
-  | CSSObject
-  | TemplateStringsArray
-  | InterpolationFunction<
-      ThemedStyledProps<Record<string, unknown>, DefaultTheme>
-    >;
+import { css } from 'styled-components';
 
-// TODO: Create own custom CSSProp in due time
-const media = Object.keys(theme.screen).reduce(
-  (acc, selector) => {
-    acc[selector as keyof typeof theme.screen] = (
-      args,
-      ...interpolations
-    ) => css`
-      @media (min-width: ${theme.screen[
-          selector as keyof typeof theme.screen
-        ]}) {
+import { theme } from './theme';
+
+const media = Object.entries(theme.screen).reduce(
+  (acc, [key, value]) => ({
+    ...acc,
+    [key]: (
+      args: TemplateStringsArray | CSSObject,
+      ...interpolations: SimpleInterpolation[]
+    ) => `
+      @media (min-width: ${value}) {
         ${css(args, ...interpolations)}
       }
-    `;
-
-    return acc;
-  },
+    `,
+  }),
   {} as {
     [key in keyof typeof theme.screen]: (
-      args: CSSPropArguments,
-      ...interpolations: Array<
-        Interpolation<ThemedStyledProps<unknown, DefaultTheme>>
-      >
-    ) => FlattenInterpolation<
-      ThemedStyledProps<Record<string, unknown>, DefaultTheme>
-    >;
+      args: TemplateStringsArray | CSSObject,
+      ...interpolations: SimpleInterpolation[]
+    ) => FlattenSimpleInterpolation;
   },
 );
 
 const mergedTheme = {
   ...theme,
+  /**
+   * The media function behaves like the styled-components css function. It
+   * allows you to write styles for media queries with the breakpoints defined
+   * in your theme.
+   * @example
+   * const MyStyledDiv = styled.div`
+   *   background: green;
+   *
+   *   ${({ theme: { media } }) => media.xl`
+   *     background: yellow;
+   *   `}
+   * `;
+   */
   media,
 };
 
--- styles/theme.ts
+++ styles/theme.ts
@@ -8,20 +8,20 @@ export const theme = {
     backgroundAlt: 'rgb(245, 245, 245)',
     primary: 'rgb(29, 223, 205)',
     primaryAlt: 'rgb(4, 51, 47)',
-  } as const,
+  },
   font: {
     primary: `Georgia, 'Times New Roman', Times, serif`,
     secondary: 'Verdana, Geneva, Tahoma, sans-serif',
-  } as const,
+  },
   screen: {
-    sm: '640px' as const,
-    md: '768px' as const,
-    lg: '1024px' as const,
-    xl: '1280px' as const,
-    xxl: '1536px' as const,
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+    xxl: '1536px',
   },
   shadow: {
     sm: '0 0 0.5rem rgba(0, 0, 0, 0.15)',
     lg: '0 0 1rem rgba(0, 0, 0, 0.15)',
-  } as const,
-};
+  },
+} as const;
